119abe149816ca6cb32fb3d86ab354eb6ef928c4
gomor <zltn.csaszar@gmail.com>
2018-10-15 21:23:05 +0200

    eulerlib

---
+++ lib/eulerlib.cpp
@@ -0,0 +1,181 @@
+#include "eulerlib.h"
+#include <string>
+#include <sstream>      // std::istringstream
+#include <fstream>
+#include <iostream>
+
+namespace eulerlib
+{
+
+	std::shared_ptr<stringVector> getAllStringFromCSV(const char* fileName)
+	{
+		std::shared_ptr<stringVector> returnList(new stringVector);
+
+		std::ifstream data(fileName);
+
+		std::string line;
+		while(std::getline(data,line))
+		{
+			std::stringstream lineStream(line);
+			std::string cell;
+			while(std::getline(lineStream,cell,','))
+			{
+				returnList->push_back(cell);
+			}
+		}
+
+		return returnList;
+	}
+
+  int getFactorial(int num)
+  {
+    if(num == 1)
+      return num;
+    else
+      return num * getFactorial(num - 1);
+  }
+
+	bool isPrime(long num)
+	{
+		if(num == 1)
+			return false;
+
+		for(long i = 2; i <= sqrt(num); i++)
+			if(num%i == 0)
+				return false;
+
+		return true;
+	}	
+
+	bool isPalindrome(long num)
+	{
+
+		int length = numberOfDigits(num);
+
+		if(length == 1)
+			return true;
+
+		int upperDigit = 0;
+		int lowerDigit = 0;
+
+		if(length % 2)
+		{
+			lowerDigit = length / 2;
+			upperDigit = lowerDigit + 2;
+		}
+		else
+		{
+			upperDigit = length / 2 + 1;
+                        lowerDigit = length / 2;
+		}
+
+		while(lowerDigit > 0)
+		{
+			if(getDigitFromPosition(num, upperDigit) != getDigitFromPosition(num, lowerDigit))
+				return false;
+
+			lowerDigit--;
+			upperDigit++;
+		}
+		
+
+		return true;
+	}
+
+	int getDigitFromPosition(long num, int pos)
+	{
+		int tenPower = 1;
+
+		for(int i = 0; i < pos - 1; i++)
+			tenPower *= 10;
+
+		num /= tenPower;
+
+		return num % 10;		
+	}
+
+        int numberOfDigits(long num)
+	{
+		int result = 0;
+
+		while(num != 0)
+		{
+			num /= 10;
+			result++;
+		}
+		return result;
+	}
+
+	int stringToInt(std::string& string)
+	{
+		int numb;
+		std::istringstream(string) >> numb;
+		return numb;
+	}
+
+	divisorMap* getDivisors(int num)
+	{
+		divisorMap* returnMap = new divisorMap;
+		if(isPrime(num))
+		{
+			(*returnMap)[num] = 1;
+			return returnMap;
+		}
+
+		int counter;
+
+		for(int i = 2; i <= sqrt(num) + 1; i++)
+		{
+			if(num%i == 0)
+			{
+				counter = 0;
+				while(num%i == 0)
+				{
+					counter++;
+					num = num / i;
+				}
+
+				(*returnMap)[i] = counter;
+	
+				if(isPrime(num))
+				{
+					(*returnMap)[num] = 1;
+					return returnMap;
+				}
+			}
+		}
+		return returnMap;
+	}
+
+	allDivisorList* getAllDivisors(int num)
+	{
+		allDivisorList* returnList = new allDivisorList;
+
+		for(int i = 1; i <= num /2; i++)
+		{
+			if(num % i == 0)
+				returnList->push_back(i);
+		}
+
+		return returnList;
+	}
+
+	int getNumOfDivisors(int num)
+	{
+		divisorMap* divisors;
+		int numOfDivisors = 1;
+
+		divisors = getDivisors(num);
+
+		for (std::map<int,int>::iterator it=(*divisors).begin(); it!=(*divisors).end(); ++it)
+                {
+                        numOfDivisors = numOfDivisors * (it->second + 1);
+
+                }
+
+		delete divisors;
+
+		return numOfDivisors;
+	
+	}
+}
---
+++ lib/eulerlib.h
@@ -0,0 +1,36 @@
+#ifndef EULERLIB_H
+#define EULERLIB_H
+
+#include <cmath>
+#include<map>
+#include<list>
+#include<vector>
+#include<string>
+#include <memory>
+
+namespace eulerlib
+{
+	bool isPrime(long num);
+	bool isPalindrome(long num);
+	int numberOfDigits(long num);
+	int getDigitFromPosition(long num, int pos);
+	int stringToInt(std::string& string);
+
+	// first int is the diviasor the second
+	// is which power the divisor is.
+	// I hope it's clear... :)
+	typedef std::map<int,int> divisorMap;
+	divisorMap* getDivisors(int num); 
+
+	typedef std::list<int> allDivisorList;
+	allDivisorList* getAllDivisors(int num); 
+
+	int getNumOfDivisors(int num);
+
+	int getFactorial(int num);
+
+	typedef std::vector<std::string> stringVector;
+	std::shared_ptr<stringVector> getAllStringFromCSV(const char* fileName); 
+}
+
+#endif
